8e583265b3d09eff0167c61f5abf2d5a0b3cde09
sukanta022 <sukantacb20@gmail.com>
2025-09-14 20:10:42 +0600

    added lesson words

--- script/index.js
+++ script/index.js
@@ -4,6 +4,13 @@ const displayLesson = () => {
         .then((data) => displayLessonData(data.data))
 }
 
+const lessonLevel = (level) =>{
+    url = `https://openapi.programming-hero.com/api/level/${level}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displayLessonWords(data.data))
+}
+
 const displayLessonData = (data) => {
     const lessonContainer = document.getElementById("lesson-container");
     
@@ -11,10 +18,27 @@ const displayLessonData = (data) => {
     data.forEach(element => {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-            <buttton class="btn btn-soft btn-primary"><i class="bi bi-book-half"></i> Lesson -${element.level_no}</button>
+            <buttton onclick ="lessonLevel(${element.level_no})" class="btn btn-soft btn-primary"><i class="bi bi-book-half"></i> Lesson -${element.level_no}</button>
         `;
         lessonContainer.append(btnDiv);
     });
+}
+
+const displayLessonWords = (data) => {
+    const lessonWordsContainer = document.getElementById("lesseonWords-container");
+    lessonWordsContainer.innerHTML = "";
+
+    data.forEach(element => {
+        const wordDiv = document.createElement("div");
+        wordDiv.innerHTML = `
+            <div id="lesson-word" class="bg-white text-center p-14 space-y-4 shadow-md h-full">
+                <p class="font-bold text-3xl">${element.word}</p>
+                <p>Meaning /Pronounciation</p>
+                <p class="font-bangla font-semibold text-3xl text-[#18181B]">"${element.meaning} / ${element.pronunciation}"</p>
+            </div>
+        `
+        lessonWordsContainer.append(wordDiv);
+    })
 }
 
 displayLesson();
